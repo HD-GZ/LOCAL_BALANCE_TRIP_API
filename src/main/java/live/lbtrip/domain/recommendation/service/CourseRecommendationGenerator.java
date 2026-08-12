8e4b036d5cf3c02456b6252fd44a2cbfb2b4d0f8
신관규 <soundbar91@gmail.com>
2026-08-12 13:55:34 +0900

    chore: 임시 커밋

--- src/main/java/live/lbtrip/domain/recommendation/service/CourseRecommendationGenerator.java
+++ src/main/java/live/lbtrip/domain/recommendation/service/CourseRecommendationGenerator.java
@@ -1,13 +1,27 @@
 package live.lbtrip.domain.recommendation.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
+import live.lbtrip.domain.propensity.model.Propensity;
+import live.lbtrip.domain.propensity.service.PropensityFinder;
+import live.lbtrip.domain.tourism.client.dto.RegionStats;
+import live.lbtrip.domain.tourism.service.RegionStatsFinder;
+import live.lbtrip.domain.tourism.service.RegionVisitorFinder;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class CourseRecommendationGenerator {
 
+    private final PropensityFinder propensityFinder;
+    private final RegionStatsFinder regionStatsFinder;
+    private final RegionVisitorFinder regionVisitorFinder;
+
     public void generate(Long userId) {
+        // init
+        Propensity propensity = propensityFinder.findByUserId(userId);
+        List<RegionStats> regionStats = regionStatsFinder.findAll();
     }
 }
